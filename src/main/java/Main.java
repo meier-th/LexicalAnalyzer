9a23a618293d4ac15854ff9f698c3de3013ef860
meier-th <steinmann.thms@gmail.com>
2020-05-31 13:21:14 +0300

    Take filename from command line arg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,12 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Lexer lexer = new Lexer(new FileReader("example"));
+        String filename;
+        if (args.length == 0)
+            filename = "example";
+        else
+            filename = args[0];
+        Lexer lexer = new Lexer(new FileReader(filename));
         YYParser parser = new YYParser(lexer);
         parser.parse();
     }
